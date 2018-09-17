0f4cf03a00dff56e37f3ef0eb5a053fbf818130e
Koy Saeteurn <kosatun@aol.com>
2018-09-17 04:19:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,17 +67,18 @@ class Dog
     dog
   end   
   
-  def self.find_by_id(id)
+   def self.find_by_id(id)
     sql = <<-SQL
       SELECT *
       FROM dogs
       WHERE id = ?
       LIMIT 1
     SQL
-    DB[:conn].execute(sql,id).execute |row|
+
+    DB[:conn].execute(sql,id).map do |row|
       self.new_from_db(row)
-    end.first  
-  end   
+    end.first
+  end  
   
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
